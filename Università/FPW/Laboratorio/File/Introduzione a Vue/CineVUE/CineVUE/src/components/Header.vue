2c9fff5bed9c7fcc21dfde3d1819c95f0064a059
Eic17H <eic17h+github@gmail.com>
2025-03-21 21:20:33 +0100

    Ad Obsidian non piace avere tutti questi file, li metto in una zip

--- Università/FPW/Laboratorio/File/Introduzione a Vue/CineVUE/CineVUE/src/components/Header.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Navbar from '../components/Navbar.vue'
-</script>
-
-<template>
-    <a href="/"><img src="../assets/png/logo.png" alt="Logo di Cineva" width="200"></a>
-      <h1>Cineva</h1>
-    <Navbar/>
-</template>
